aedc7b5a2577c66a28b6164485605eb31d80e147
BlablaTara <tara0561@stud.kea.dk>
2025-02-26 21:04:35 +0100

    Its done

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require ('express');
+const path = require('path');
+
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'punctualityTest.html'));
+});
+
+
+const PORT = 8080;
+app.listen(PORT, (error) => {
+    if (error) {
+        console.log("Error starting server", error);
+        return;
+    }
+    console.log("Server is running on port: ", PORT);
+});
+
